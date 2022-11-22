6f4bcef1ebeadc1232a6558d8818c3726cac3d3e
Farzpal Singh <sharma.paras4444@gmail.com>
2022-11-22 19:15:57 +0530

    remove useEffect

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Airlines from './Airlines/Airlines'
 import Airline from './Airlines/[id]'
@@ -10,7 +10,6 @@ const App = () => {
     const cookieArr = document.cookie.split(';')
     for (let i = 0; i < cookieArr.length; i++) {
       const cookiePair = cookieArr[i].split('=')
-
       if (name == cookiePair[0].trim()) {
         return decodeURIComponent(cookiePair[1])
       }
