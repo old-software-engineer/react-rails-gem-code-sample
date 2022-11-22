9d65665747c50dad3ce3969382c3ee5370f2fc87
Farzpal Singh <sharma.paras4444@gmail.com>
2022-11-22 21:28:11 +0530

    Added Loader Spinner

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,63 +1,81 @@
-import React, { useState, useEffect } from 'react'
-import { airlinesRender } from '../../api/AirlineApi'
-import Slider from 'react-slick'
-import { Header } from './Airlines.style'
-import AirlineCard from './AirlineCard'
+import React, { useState, useEffect } from "react";
+import { airlinesRender } from "../../api/AirlineApi";
+import Slider from "react-slick";
+import { Header } from "./Airlines.style";
+import AirlineCard from "./AirlineCard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const SETTINGS = {
-  className: 'center',
+  className: "center",
   centerMode: true,
   dots: true,
   focusOnSelect: true,
   infinite: true,
   speed: 500,
   slidesToShow: 3,
-  slidesToScroll: 1
-}
+  slidesToScroll: 1,
+};
 
 const Airlines = ({ token }) => {
-  const [airlines, setAirlines] = useState([]) // Airlines data
-  const [activeSlide, setActiveSlide] = useState(0)
-  const [bgImage, setBgImage] = useState('/assets/flight.jpg')
+  const [airlines, setAirlines] = useState([]); // Airlines data
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [bgImage, setBgImage] = useState("/assets/flight.jpg");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     airlinesRender(token)
       .then((resp) => {
-        setAirlines(resp.data.data)
+        setAirlines(resp.data.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 500);
       })
       .catch((error) => {
-        console.log('Error', error)
-      })
-  }, [])
+        toast.error("Something went wrong!");
+        setLoading(false);
+      });
+  }, []);
 
   const sliderSettings = {
     ...SETTINGS,
-    beforeChange: (current, next) => setActiveSlide(next)
-  }
+    beforeChange: (current, next) => setActiveSlide(next),
+  };
 
   useEffect(() => {
-    airlines.length && setBgImage(airlines[activeSlide].attributes.image_url)
-  }, [activeSlide])
+    airlines.length && setBgImage(airlines[activeSlide].attributes.image_url);
+  }, [activeSlide]);
 
   return (
-    <div
-      className='home'
-      style={{
-        backgroundImage: `url(${bgImage})`
-      }}
-    >
-      <Header />
-      <Slider {...sliderSettings}>
-        {airlines.map((item, index) => (
-          <AirlineCard
-            key={index}
-            attributes={item.attributes}
-            activeSlide={activeSlide === index}
-          />
-        ))}
-      </Slider>
+    <div>
+      <ToastContainer theme="dark" closeOnClick position="bottom-right" />
+      {loading ? (
+        <div className="loader-container" id="background-cover">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <div
+          className="home"
+          style={{
+            backgroundImage: `url(${bgImage})`,
+          }}
+        >
+          {" "}
+          <Header />
+          <Slider {...sliderSettings}>
+            {airlines.map((item, index) => (
+              <AirlineCard
+                key={index}
+                attributes={item.attributes}
+                activeSlide={activeSlide === index}
+              />
+            ))}
+          </Slider>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Airlines
+export default Airlines;
--- app/javascript/components/Auth/Login.js
+++ app/javascript/components/Auth/Login.js
@@ -1,86 +1,107 @@
-import React, { useState, useEffect } from 'react'
-import { signIn } from '../../api/AirlineApi'
+import React, { useState, useEffect } from "react";
+import { signIn } from "../../api/AirlineApi";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Login = ({ setToken, setLogin }) => {
-  const [user, setUser] = useState({ email: '', password: '' })
-  const [enable, setEnable] = useState(false)
-  const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [enable, setEnable] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   const handleChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value })
-  }
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
   useEffect(() => {
-    setEnable(reg.test(user.email) && user.password.length > 5)
-  }, [user])
+    setEnable(reg.test(user.email) && user.password.length > 5);
+  }, [user]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    setLoading(true);
+    e.preventDefault();
     signIn(user)
       .then((resp) => {
-        setToken(resp.headers.authorization)
-        document.cookie = `token=${resp.headers.authorization}`
+        setLoading(false);
+        setEnable(false);
+        setToken(resp.headers.authorization);
+        document.cookie = `token=${resp.headers.authorization}`;
       })
       .catch((error) => {
-        console.log('error', error)
-      })
-  }
+        console.log("error", error);
+        setLoading(false);
+        toast.error("Something went wrong!");
+      });
+  };
 
   return (
-    <div id='background-cover' className='section'>
-      <div className='section-center'>
-        <div className='container'>
-          <div className='user-form' style={{ maxWidth: '768px' }}>
+    <div id="background-cover" className="section">
+      <ToastContainer theme="dark" closeOnClick position="bottom-right" />
+      <div className="section-center">
+        <div className="container">
+          <div className="user-form" style={{ maxWidth: "768px" }}>
             <h1
-              className='text-white'
-              style={{ display: 'flex', justifyContent: 'center' }}
+              className="text-white"
+              style={{ display: "flex", justifyContent: "center" }}
             >
               Sign In
             </h1>
             <form onSubmit={handleSubmit}>
-              <div className='field-input'>
-                <span className='form-label'>Email</span>
+              <div className="field-input">
+                <span className="form-label">Email</span>
                 <input
                   onChange={handleChange}
-                  type='email'
-                  placeholder='Email'
-                  name='email'
+                  type="email"
+                  placeholder="Email"
+                  name="email"
                   value={user.email}
                 />
               </div>
-              <div className='field-input'>
-                <span className='form-label'>Password</span>
+              <div className="field-input">
+                <span className="form-label">Password</span>
                 <input
                   onChange={handleChange}
-                  type='password'
-                  placeholder='password'
-                  name='password'
+                  type="password"
+                  placeholder="password"
+                  name="password"
                   value={user.password}
                 />
               </div>
               <div
-                className='row'
+                className="row"
                 style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  textAlign: 'center'
+                  display: "flex",
+                  flexDirection: "column",
+                  textAlign: "center",
                 }}
               >
-                <div style={{ padding: '0', margin: '1rem 0' }}>
+                <div
+                  style={{
+                    padding: "0",
+                    margin: "1rem 0",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
                   <button
-                    type='submit'
-                    style={{ width: '20rem' }}
+                    type="submit"
+                    style={{
+                      width: "20rem",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
                     disabled={!enable}
-                    className={!enable ? 'submit-btn-disabled' : 'submit-btn'}
+                    className={!enable ? "submit-btn-disabled" : "submit-btn"}
                   >
-                    Submit
+                    {loading ? <div className="spinner-login"></div> : "Submit"}
                   </button>
                 </div>
               </div>
             </form>
             <a
               onClick={() => {
-                setLogin(false)
+                setLogin(false);
               }}
-              className='text-white link-center'
+              className="text-white link-center"
             >
               Register
             </a>
@@ -88,7 +109,7 @@ const Login = ({ setToken, setLogin }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
